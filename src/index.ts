bea59d6425fe223e69f4a99c7c841c2c72ae705c
frastsytt <frastsytt@gmail.com>
2023-02-21 14:44:08 +0200

    ts, routing

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Koa from "koa";
+import Router from "koa-router";
+import logger from "koa-logger";
+import json from "koa-json";
+
+const app = new Koa();
+const router = new Router();
+
+
+router.get("/", async (ctx, next) => {
+  ctx.body = { msg: "helao"}
+})
+
+
+app.use(json());
+app.use(logger());
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(443, () => {
+  console.log('Serving on port 443')
+})
