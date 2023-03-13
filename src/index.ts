48d18c35f71d36da04818eb190c2dd3a3f13f6a8
frastsytt <frastsytt@gmail.com>
2023-03-13 21:54:59 +0200

    authorization middleware, key output

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,24 @@ import json from "koa-json";
 const app = new Koa();
 const router = new Router();
 
+const authenticationCheck = (ctx: Context, next: Next) => {
+  const authHeader = ctx.headers.authorization;
+  if (!authHeader || authHeader !== process.env.SECRET) {
+    ctx.throw(401, "Unauthorized");
+  }
+  return next();
+}
+
 router.get("/", async (ctx, next) => {
-  ctx.body = { msg: "helao"}
+  ctx.body = { msg: "Hello World!"}
+})
+
+router.get("/secret", authenticationCheck, async (ctx, next) => {
+  ctx.body = { msg: "You're authorized"}
+})
+
+router.get("/generatekey", async (ctx, next) => {
+  ctx.body = { key: process.env.SECRET}
 })
 
 
@@ -18,6 +34,6 @@ app.use(logger());
 
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(443, () => {
-  console.log('Serving on port 443')
+app.listen(process.env.PORT, () => {
+  console.log(`Serving on port ${process.env.PORT}`)
 })
